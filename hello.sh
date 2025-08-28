0d8f56dd9803d12f900397b1a894da77b5fb9cf9
srinuyaram <srinuvasuluy.vtalent@gmail.com>
2025-08-28 14:43:42 +0530

    shellcheck

---
+++ hello.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+echo "hello!"
+
+P1="Ramesh"
+P2="Suresh"
+
+echo "$P1: Hi ! $P2 how are you?"
+echo "$P2: I am fine. Thankyou! how are you  $P1"
+echo "$P1: I am doing well..$P2"
+
+N1=100
+N2=120
+
+sum=$N1+$N2
+
+echo "sum of $N1 and $N2=$sum" 
