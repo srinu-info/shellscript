7e1d827c4007ad0e6b08dd85a4642d1c4d0a4e80
srinuyaram <srinuvasuluy.vtalent@gmail.com>
2025-08-29 14:38:06 +0530

    condition check

--- condition.sh
+++ condition.sh
@@ -5,8 +5,17 @@ USERID=$(id -u)
 if [ $USERID -ne 0 ]
 then
  echo "Error-run with root user"
+ exit 1
  else
  echo "you are the root user"
 fi
-echo "hello"
-dnf install mysql -y
+
+dnf install mysql11111 -y
+
+if [ $? eq 0 ]
+then 
+    echo " MySQl installing Success.."
+    else
+    echo "Installation Failure.."
+
+fi
