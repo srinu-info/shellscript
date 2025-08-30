2b6308b9fea2102ede69f22529c1acc86af17c48
srinuyaram <srinuvasuluy.vtalent@gmail.com>
2025-08-30 15:10:27 +0530

    conditions

--- execution.sh
+++ execution.sh
@@ -5,22 +5,29 @@ R="\e[31m"
 G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
+LOG_FOLDER="/var/logs/shellscript-logs"
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+Log_file="$LOG_FOLDER/$SCRIPT_NAME.log"
+
+mkdir -p $LOG_FOLDER
+echo "Script started executing at: $(date)"  &>>Log_file
+
 
 if [ $USERID -ne 0 ]
 then
-echo -e "$R You are not root user,Please try with root user.-- $N"
+echo -e "$R You are not root user,Please try with root user.-- $N" &>>Log_file
 exit 1
 else
-echo -e "$G You are root user---$N"
+echo -e "$G You are root user---$N" &>>Log_file
 fi
 
 #----
 VALIDATE(){
     if [ $1 -eq 0 ]  
 then
-echo -e  "$G Successfully installed $2---$N"
+echo -e  "$G Successfully installed $2---$N" &>>Log_file
 else
-echo -e "$R $2 Installation failed---$N"
+echo -e "$R $2 Installation failed---$N" &>>Log_file
 exit 1
 fi    
 }
@@ -30,11 +37,11 @@ fi
 dnf list installed mysql
 if [ $? -ne 0 ]
 then 
-echo -e "$Y Installing MySQL wait----$N"
+echo -e "$Y Installing MySQL wait----$N " &>>Log_file
 dnf install mysql -y
 VALIDATE $? "mysql"
 else
-echo -e "$Y Already MySql Installed---$N"
+echo -e "$Y Already MySql Installed---$N" &>>Log_file
 fi
 
 #---installation of nginx
@@ -42,11 +49,11 @@ fi
 dnf list installed nginx
 if [ $? -ne 0 ]
 then 
-echo -e "$Y Installing nginx wait----$N"
+echo -e "$Y Installing nginx wait----$N" &>>Log_file
 dnf install nginx -y
 VALIDATE $? "nginx"
 else
-echo -e "$Y Already nginx Installed--$N"
+echo -e "$Y Already nginx Installed--$N" &>>Log_file
 fi
 
 #---installation of git
@@ -54,12 +61,12 @@ fi
 dnf list installed git
 if [ $? -ne 0 ]
 then 
-echo -e "$Y Installing git wait----$N"
+echo -e "$Y Installing git wait----$N" &>>Log_file
 
 dnf install git -y
 VALIDATE $? "git"
 else
-echo -e "$Y Already git Installed--$N"
+echo -e "$Y Already git Installed--$N" &>>Log_file
 fi
 
 
