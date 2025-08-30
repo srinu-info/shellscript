1c9422c1a00a2849a6fa3317b1fbd61867d7c832
srinuyaram <srinuvasuluy.vtalent@gmail.com>
2025-08-30 13:25:04 +0530

    conditions

---
+++ execution.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+USERID=$(id -u)
+
+if [ $USERID -ne 0 ]
+then
+echo "You are not root user,Please try with root user."
+exit 1
+else
+echo "You are root user"
+fi
+
+dnf install mysql -y
+
+if [ $? -eq 0]
+then
+echo "iSuccessfully installed MySql"
+else
+echo "MySql Installation failed"
+exit 1
+fi
+
