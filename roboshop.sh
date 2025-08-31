e389c61ebc288c9cbfcef7fc60fda9f05144eb98
srinuyaram <srinuvasuluy.vtalent@gmail.com>
2025-08-31 14:14:38 +0530

    shell robo

--- roboshop.sh
+++ roboshop.sh
@@ -10,7 +10,7 @@ DOMAIN_ID="svdvps.online"
 for instances in ${INSTANCES[@]}
 do
 INSTANCE_ID=$(aws ec2 run-instances   --image-id ami-09c813fb71547fc4f  -instance-type t3.micro   --security-group-ids sg-0771b87321713642a  --tag-specifications "ResourceType=instance,  Tags=[{Key=Name,Value=$instances}]" --query "Instances[0].InstanceId"   --output text)
-if [ $instnace !="frontend" ] 
+if [ $instnace != "frontend" ] 
 then
 IP=$(aws ec2 describe-instances   --instance-ids $INSTANCE_ID   --query "Reservations[0].Instances[0].PrivateIpAddress" --output text)
   else
