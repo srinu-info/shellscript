7648859c81bc1a1750b65319bb34cbbacc09e0c4
srinu-info <sriuvasuluyaram@gmail.com>
2026-07-24 22:02:14 +0530

    shell practice

--- 1-sample.sh
+++ 1-sample.sh
@@ -6,26 +6,38 @@ G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
 
+LOGS_FOLDER="/var/log/shell-log"
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
+
+mkdir -p $LOGS_FOLDER
+echo "script executed at: $(date)" &>>$LOG_FILE
+
 if [ $USERID -ne 0 ]
 then
-	echo -e "$R ERROR: You dont have access $N"
+	echo -e "$R ERROR: You dont have access $N" &>>$LOG_FILE
 	exit 1
 else
-	echo -e "$G You have root access $N"
+	echo -e "$G You have root access $N" &>>$LOG_FILE
 fi
+
+VALIDATE(){
+	if [ $1 -eq 0 ]
+	then
+		echo -e "nginx installed $G Successfully $N" &>>$LOG_FILE
+	else
+		echo -e "$G nginx installation failed..$N" &>>$LOG_FILE
+		exit 1
+	fi
+
+}
 
 dnf list installed nginx
 if [ $? -ne 0 ] 
 then 
-	echo -e " $G nginx not available.. Installing now...$N"
+	echo -e " $G nginx not available.. Installing now...$N" &>>$LOG_FILE
 	dnf install nginx -y
-	if [ $? -eq 0 ]
-	then
-		echo -e "nginx installed $G Successfully $N"
-	else
-		echo -e "$G nginx installation failed..$N"
-		exit 1
-	fi
+	VALIDATE $? "nginx"
 else
-	echo -e " $Y  nginx already installed $N"
+	echo -e " $Y  nginx already installed $N" &>>$LOG_FILE
 fi
