f80b7a94a42ee8980bf12cc21ab84520d28a9852
srinu-info <sriuvasuluyaram@gmail.com>
2026-07-25 12:06:31 +0530

    shell

--- 1-sample.sh
+++ 1-sample.sh
@@ -11,32 +11,32 @@ SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
 LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
 
 mkdir -p $LOGS_FOLDER
-echo "script executed at: $(date)" &>>$LOG_FILE
+echo "script executed at: $(date)" 
 
 if [ $USERID -ne 0 ]
 then
-	echo -e "$R ERROR: You dont have access $N" &>>$LOG_FILE
+	echo -e "$R ERROR: You dont have access $N" 
 	exit 1
 else
-	echo -e "$G You have root access $N" &>>$LOG_FILE
+	echo -e "$G You have root access $N" 
 fi
 
 VALIDATE(){
 	if [ $1 -eq 0 ]
 	then
-		echo -e "$2 installed $G Successfully $N" &>>$LOG_FILE
+		echo -e "$2 installed $G Successfully $N" 
 	else
-		echo -e "$G $2 installation failed..$N" &>>$LOG_FILE
+		echo -e "$G $2 installation failed..$N" 
 		exit 1
 	fi
 }
 
-dnf list installed nginx &>>$LOG_FILE
+dnf list installed nginx 
 if [ $? -ne 0 ] 
 then 
-	echo -e " $G nginx not available.. Installing now...$N" &>>$LOG_FILE
-	dnf install nginx -y &>>$LOG_FILE
+	echo -e " $G nginx not available.. Installing now...$N" 
+	dnf install nginx -y 
 	VALIDATE $? "nginx"
 else
-	echo -e " $Y  nginx already installed $N" &>>$LOG_FILE
+	echo -e " $Y  nginx already installed $N" 
 fi
