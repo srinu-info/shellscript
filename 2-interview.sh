7a82ccb4e650412bfda4626519530e46701f9b8e
srinu-info <sriuvasuluyaram@gmail.com>
2026-07-25 20:11:06 +0530

    shell

--- 2-interview.sh
+++ 2-interview.sh
@@ -1,29 +1,40 @@
 #!/bin/bash
 
-servers=("nginx" "git")
+# servers=("nginx" "git")
 
-for server in "${servers[@]}"
-do 
-	systemctl is-active --quiet $service 
-done
+# for server in "${servers[@]}"
+# do 
+# 	systemctl is-active --quiet $service 
+# done
 
-echo "------------------------------"
+# echo "------------------------------"
 
-echo "find error in file"
- grep Error /var/log/shell-log/1-sample.log
+# echo "find error in file"
+#  grep Error /var/log/shell-log/1-sample.log
 
- echo "-----------------------------"
+#  echo "-----------------------------"
 
-service=nginx
+# service=nginx
 
-if  ! systemctl is-active --quiet $service 
-then
-systemctl restart $service
-else
-echo "service is Active"
-fi
-echo "--------------------------------"
+# if  ! systemctl is-active --quiet $service 
+# then
+# systemctl restart $service
+# else
+# echo "service is Active"
+# fi
+# echo "--------------------------------"
 
-count=$(ls | wc -l)
-echo $count
+# count=$(ls | wc -l)
+# echo $count
 
+while IFS=read -r line
+do
+	echo $line
+done < cart.log
+echo "-----------------------"
+SOURCE="/home/ec2-user/app-logs"
+FILE=$(find $SOURCE -type f -mtime +14)
+while IFS= read -r filepath
+do
+	rm -rf $filepath
+done <<< $FILE
