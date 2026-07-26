5da4e9aa3b82e3538ded374a31b50c95b7a211d5
srinu-info <sriuvasuluyaram@gmail.com>
2026-07-26 17:28:25 +0530

    shell

--- 2-interview.sh
+++ 2-interview.sh
@@ -14,15 +14,15 @@
 
 #  echo "-----------------------------"
 
-# service=nginx
+service=nginx
 
-# if  ! systemctl is-active --quiet $service 
-# then
-# systemctl restart $service
-# else
-# echo "service is Active"
-# fi
-# echo "--------------------------------"
+if  ! systemctl is-active --quiet $service 
+then
+systemctl restart $service
+else
+echo "service is Active"
+fi
+echo "--------------------------------"
 
 # count=$(ls | wc -l)
 # echo $count
@@ -40,41 +40,42 @@
 # 	rm -rf $filepath
 # done <<< $FILE
 
-# echo "-----------------------"
+echo "-----------------------"
 
-SOURCE=$1
-DEST=$2
-DAYS=${3:-14}
-if [ ! -d "$SOURCE" ] || [ ! -d "$DEST" ]
-then
-	echo "Source or Dest directory not exits"
-	exit 1
-fi
-FILES=$(find $SOURCE -type f -name "*.log" -mtime +$DAYS)
+# SOURCE=$1
+# DEST=$2
+# DAYS=${3:-14}
+# if [ ! -d "$SOURCE" ] || [ ! -d "$DEST" ]
+# then
+# 	echo "Source or Dest directory not exits"
+# 	exit 1
+# fi
+# FILES=$(find $SOURCE -type f -name "*.log" -mtime +$DAYS)
 
-if [ -n "$FILES" ]
-then
-	TIMESTAMP=$(date +%F-%H-%M-%S)
-	ZIP_FILE="$DEST/app-logs-$TIMESTAMP.zip"
-	find $SOURCE -name "*.log" -mtime +$DAYS | zip -@ "$ZIP_FILE"
+# if [ -n "$FILES" ]
+# then
+# 	TIMESTAMP=$(date +%F-%H-%M-%S)
+# 	ZIP_FILE="$DEST/app-logs-$TIMESTAMP.zip"
+# 	find $SOURCE -name "*.log" -mtime +$DAYS | zip -@ "$ZIP_FILE"
 
-	if [ -f $ZIP_FILE ]
-	then
-		echo "FILES BACKUP SUCCESSFULL"
-		echo "REMOVING FILES"
-		while IFS= read -r filepath
-		do
-			rm -rf $filepath
-		done <<< $FILES
-	else
-		echo "ZIP FAILED"
-		exit 1
-	fi
-else
-echo "Files not found"
-exit 1
-fi
+# 	if [ -f $ZIP_FILE ]
+# 	then
+# 		echo "FILES BACKUP SUCCESSFULL"
+# 		echo "REMOVING FILES"
+# 		while IFS= read -r filepath
+# 		do
+# 			rm -rf $filepath
+# 		done <<< $FILES
+# 	else
+# 		echo "ZIP FAILED"
+# 		exit 1
+# 	fi
+# else
+# echo "Files not found"
+# exit 1
+# fi
 	
-
+# echo "-----------------------"
+# SERVER=(nginx)
 
 
