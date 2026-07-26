41fc4387ea3f39d46ef50a8fc1a8d6403bfdc85e
srinu-info <sriuvasuluyaram@gmail.com>
2026-07-26 15:50:23 +0530

    shell

--- 3-usage.sh
+++ 3-usage.sh
@@ -2,11 +2,11 @@
 THRESHOLD=70
 DISK=$(df -hP | tail -n +2)
 
-while IFS= read -r filesystem size used avail use mount
+df -hP | tail -n +2 | while read -r filesystem size used avail use mount
 do
 	USAGE=${use%\%}
 	if [ "$USAGE" -ge "$THRESHOLD" ]
 	then
 		echo "ALERT: Disk Usage on $mount is ${USAGE}%"
 	fi
-done <<< $DISK
+done 
