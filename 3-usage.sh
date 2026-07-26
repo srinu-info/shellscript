6d2dc9e66ebe209effb97fa59072dc0b2c74966d
srinu-info <sriuvasuluyaram@gmail.com>
2026-07-26 16:19:51 +0530

    shell

--- 3-usage.sh
+++ 3-usage.sh
@@ -1,24 +1,34 @@
 #!/bin/bash
-THRESHOLD=70
-DISK=$(df -hP | tail -n +2)
+# THRESHOLD=70
+# DISK=$(df -hP | tail -n +2)
 
 
-df -hP | tail -n +2 | while read -r filesystem size used avail use mount
-do
-	USAGE=${use%\%}
-	if [ "$USAGE" -ge "$THRESHOLD" ]
-	then
-		echo "ALERT: Disk Usage on $mount is ${USAGE}%"
-	fi
-done 
-echo "----------------"
-DISKSPACE=$(df -hT | grep -v Filesystem)
-while IFS= read line
-do
-	USE=$(echo $line | awk '{print $6F}'| cut -d "%" -f1)
-	MOUNT=$(echo $line | awk '{print $7F}' )
-	if [ $USE -ge $THRESHOLD ]
-	then
-		echo "ALERT: Disk Usage on $MOUNT is $USE%"
-	fi
-done <<< $DISKSPACE
+# df -hP | tail -n +2 | while read -r filesystem size used avail use mount
+# do
+# 	USAGE=${use%\%}
+# 	if [ "$USAGE" -ge "$THRESHOLD" ]
+# 	then
+# 		echo "ALERT: Disk Usage on $mount is ${USAGE}%"
+# 	fi
+# done 
+# echo "----------------"
+# DISKSPACE=$(df -hT | grep -v Filesystem)
+# while IFS= read line
+# do
+# 	USE=$(echo $line | awk '{print $6F}'| cut -d "%" -f1)
+# 	MOUNT=$(echo $line | awk '{print $7F}' )
+# 	if [ $USE -ge $THRESHOLD ]
+# 	then
+# 		echo "ALERT: Disk Usage on $MOUNT is $USE%"
+# 	fi
+# done <<< $DISKSPACE
+echo "------------***************---------------------"
+THRESHOLD=30
+free -h | awk "/Mem:/"
+
+MEMORY=$($3/$2*100)
+
+if [ "$MEMORY" -ge "$THRESHOLD "]
+then
+	echo "ALERT: Memory is FULL"
+fi
