933fc9b7d13d9e91c780e6cfcff8231fb719c581
srinu-info <sriuvasuluyaram@gmail.com>
2026-07-26 15:48:32 +0530

    shell

--- 3-usage.sh
+++ 3-usage.sh
@@ -2,7 +2,7 @@
 THRESHOLD=70
 DISK=$(df -hP | tail -n +2)
 
-while IFS= read line -r filesystem size used avail use mount
+while IFS= read -r filesystem size used avail use mount
 do
 	USAGE=${use%\%}
 	if [ "$USAGE" -ge "$THRESHOLD" ]
